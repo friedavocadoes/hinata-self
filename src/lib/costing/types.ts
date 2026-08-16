f736cc4516f720fe732ae3ef7a9c7c01f763bf08
gautham <desertncamel@gmail.com>
2026-08-16 17:45:42 +0400

    refactor: model costing from purchase lots

--- src/lib/costing/types.ts
+++ src/lib/costing/types.ts
@@ -1,118 +1,78 @@
 export interface CostingInput {
   productId: string;
-
   quantityKg: number;
-
   deliveryType: "local" | "export";
-
   destinationId: string;
-
   incoterm: string;
-
   paymentTerm: string;
-
   creditDays: number;
-
   targetProfitPct: number;
-
   vehicleType?: string;
+  freightAed?: number;
+}
 
-  finalMarginPct?: number;
+export interface PurchaseBatch {
+  purchaseItemId: string;
+  purchaseNumber: string;
+  purchaseOrderId: string;
+  quantityAvailableKg: number;
+  quantityOriginalKg: number;
+  volumeCbm: number;
+  receivedAt: string;
+  unitExWorksCostAed: number;
+  purchaseOrderExWorksCost: number;
+  purchaseSharedCost: number;
+  storageRateAedPerCbmDay: number;
+}
+
+export interface IncotermCostRule {
+  cost_code: string;
+  enabled: boolean;
+  calculation_type: "manual" | "fixed" | "percentage" | "disabled";
+  amount_aed: number;
+  rate_pct: number;
+  base_code: "ex_works" | "purchase_value" | "sales_value" | "quantity" | "manual" | null;
+  multiplier: number;
 }
 
 export interface CostingResult {
   quantityKg: number;
+  sourcePurchaseCount: number;
+  purchaseCost: number;
+  purchaseUnitCost: number;
+  warehouseDays: number;
 
   exWorksCost: number;
   supplierInvoiceValue: number;
-
   inwardClearance: number;
   inwardBankCharge: number;
   storageCharge: number;
-
   outwardClearance: number;
   outwardTransport: number;
-
   freight: number;
   insurance: number;
   otherExpense: number;
-
   bankFinanceCharge: number;
   capitalInterest: number;
   customsDuty: number;
 
   totalCost: number;
   costPerUnit: number;
-
   salesPrice: number;
   salesUnitPrice: number;
-
   profitAmount: number;
   finalMarginPct: number;
 }
 
-export interface CostingProduct {
-  id?: string;
-
-  name?: string;
-
-  pack_size_kg: number;
-
-  cif_rate_usd: number;
-
-  default_profit_pct: number;
-
-  inward_clearance_charge: number;
-
-  storage_rate: number;
-
-  storage_days: number;
-
-  supplier_id?: string | null;
-
-  supplier_mop: string | null;
-
-  supplier_credit_days: number;
-}
-
 export interface CostingSettings {
-  usd_to_aed_conversion: number;
-
   annual_interest_rate: number;
-
-  customs_duty_pct: number;
-
-  tt_bank_flat_fee: number;
-
-  da_bank_fee_pct: number;
-
-  lc_sight_pct: number;
-  lc_30_pct: number;
-  lc_60_pct: number;
-  lc_90_pct: number;
-  lc_120_pct: number;
-
   customer_da_flat_fee: number;
-
   customer_lc_flat_fee: number;
-
   customer_lc_value_pct: number;
-
   customer_bank_draft_flat_fee: number;
-
-  marine_insurance_base_pct: number;
-
-  marine_insurance_cif_multiplier: number;
-
-  marine_insurance_fob_multiplier: number;
-
   courier_local_aed: number;
-
   courier_gcc_aed: number;
-
   courier_africa_aed: number;
-
   dwc_surcharge_under_7t: number;
-
   dwc_surcharge_over_7t: number;
 }
